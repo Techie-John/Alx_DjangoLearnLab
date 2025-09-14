f5e6d6feb7cdfc7390fdfab52fe77e4295dddbf6
John Ayodele <ayodelejohn40@gmail.com>
2025-09-15 00:02:45 +0100

    new

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,4 +5,7 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
-        
+
+class ExampleForm(forms.Form):
+    your_name = forms.CharField(label="Your name", max_length=100)
+    email = forms.EmailField(label="Your email")        
