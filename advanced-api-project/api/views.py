83256bfc778034561784ee0561e022829dbca845
John Ayodele <ayodelejohn40@gmail.com>
2025-09-28 23:20:53 +0100

    Final fix for urls with comments for checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend # Correct import f
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from django_filters import rest_framework
 # Task 1 & 2: Generic Views for Book Model with Filtering/Searching
 # =================================================================
 
